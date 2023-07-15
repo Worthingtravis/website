f9861d841c1b24af7415f48bae86c50f586d9669
laughingwhales <worthingtravis@gmail.com>
2023-07-15 15:35:31 -0700

    ✨ feat(useLocalStorage): Add useLocalStorage hook
    🔧 refactor(usePopperTooltip): Move usePopperTooltip to hooks folder
    
    🧪 test(usePopperTooltip): Move usePopperTooltip.test to hooks folder

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export const useLocalStorage = (key: string, initialValue: string) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      // Get from local storage by key
+      const item =
+        typeof window !== 'undefined' && window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error return initialValue
+      // eslint-disable-next-line no-console
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: string) => {
+    try {
+      // Save to local storage
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
