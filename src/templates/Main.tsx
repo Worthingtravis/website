8c696eba1a4cbea44ab6fbcc6847196dd17b393a
laughingwhales <worthingtravis@gmail.com>
2023-10-19 08:53:38 -0700

    fix prettier

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -144,7 +144,7 @@ const Main = (props: IMainProps) => {
         </footer>
       </div>
       {/* eslint-disable-next-line tailwindcss/no-contradicting-classname */}
-      <div className="fixed top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+      <div className="fixed top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]" />
     </CardSpotlightEffect>
   );
 };
